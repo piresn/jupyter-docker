1914bd953b0258e26c66540618aacd1aa55a851f
Nuno Pires <nuno.d.pires@gmail.com>
2020-08-13 13:25:08 +0200

    define resources

--- createDroplet.sh
+++ createDroplet.sh
@@ -2,35 +2,45 @@
 
 source .env
 
-dropletNAME=docker-$(date | tr -d " " | tr -d :)
+CPU=1
+MEM=1
 
-dropletID=$(curl -X POST "https://api.digitalocean.com/v2/droplets" \
-	-d '{"name":"'$dropletNAME'","region":"'$REGION'","size":"s-1vcpu-1gb","image":"docker-18-04","ssh_keys":["'$FINGERPRINT'"]}' \
+dropletSIZE=$(echo s-$CPU vcpu- $MEM gb | tr -d ' ')
+dropletNAME=docker-$(date | tr -d " " | tr -d :)
+dropletIMAGE=docker-18-04
+
+echo creating droplet $dropletNAME...
+
+dropletID=$(curl -s -X POST "https://api.digitalocean.com/v2/droplets" \
+	-d '{"name":"'$dropletNAME'","region":"'$REGION'","size":"'$dropletSIZE'","image":"'$dropletIMAGE'","ssh_keys":["'$FINGERPRINT'"]}' \
 	-H "Authorization: Bearer $TOKEN" \
 	-H "Content-Type: application/json" | jq '.droplet.id')
 
-echo creating droplet with id $dropletID...
+echo droplet ID = $dropletID
 
 sleep 10
 
 # retrieve ip
-dropletIP=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets/$dropletID" | jq '.droplet.networks.v4[0].ip_address' | tr -d \")
+dropletIP=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets/$dropletID" | jq '.droplet.networks.v4[0].ip_address' | tr -d \")
 
 echo droplet IP = $dropletIP
 echo Preparing droplet...
+
+sleep 10
 
 scp -o StrictHostKeyChecking=no -o ServerAliveCountMax=20 -r docker/ root@$dropletIP:/root
 ssh root@$dropletIP "cd docker; docker build -t jup .; docker run -d -p 8888:8888 --name labcont jup;"
 ssh -L 8888:localhost:8888 -f -N root@$dropletIP
 
+
 echo '++++++++++++++++++++'
 echo Finished. Jupyter lab is now running at http://localhost:8888/lab
-echo '++++++++++++++++++++'
+echo 
 echo To stop portforwarding, use "pkill ssh" directly.
 echo
 echo To destroy droplet, use
-echo
-echo source .env
-echo curl -X DELETE -H \"Content-Type: application/json\" -H \"Authorization: Bearer \$TOKEN\" \"https://api.digitalocean.com/v2/droplets/$dropletID\"
-echo
+echo source .env > removeLastDroplet
+echo curl -X DELETE -H \"Content-Type: application/json\" -H \"Authorization: Bearer \$TOKEN\" \"https://api.digitalocean.com/v2/droplets/$dropletID\" >> removeLastDroplet
+cat removeLastDroplet
+echo rm removeLastDroplet >> removeLastDroplet
 echo '++++++++++++++++++++'
